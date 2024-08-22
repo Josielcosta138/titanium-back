1441d8dec0a68f2880e30c4091c871731258b521
Josielcosta138 <63210548+Josielcosta138@users.noreply.github.com>
2024-08-21 21:08:09 -0300

    primeiro commit

---
+++ src/main/java/com/br/titanium/TitaniumApplication.java
@@ -0,0 +1,13 @@
+package com.br.titanium;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TitaniumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TitaniumApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/titanium/TitaniumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.titanium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TitaniumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
